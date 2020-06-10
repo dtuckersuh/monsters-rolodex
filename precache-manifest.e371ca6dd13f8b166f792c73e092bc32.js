725fa99fef4e6b75c7f74f68d87362a6a1639cee
Daniel Suh <suh977@gmail.com>
2020-06-10 16:56:34 -0700

    Updates

---
+++ precache-manifest.e371ca6dd13f8b166f792c73e092bc32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5da2c776cda713a9cc6e621534198bc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c636c90c86328f5658c",
+    "url": "/monsters-rolodex/static/css/main.107db47a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c636c90c86328f5658c",
+    "url": "/monsters-rolodex/static/js/main.c0256549.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
